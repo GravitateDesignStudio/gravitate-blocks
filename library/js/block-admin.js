29ef2df72220156de8a55ddbfd5f81541d0089b9
Doug Frei <doug.frei@gravitatedesign.com>
2017-11-09 16:50:42 -0800

    fixed admin JS issue with multiple block sections; added comments for future developers

--- library/js/block-admin.js
+++ library/js/block-admin.js
@@ -6,22 +6,31 @@ jQuery(document).ready(function($)
     gravBlocks = {};
 
     gravBlocks.init = function(){
-        var $gravpopup = $( $('#acf-group_grav_blocks .tmpl-popup').html() );
+        // Loop through all block list "popup" templates
+        // 
+        // These are the dropdown lists that appear when clicking "Add Content"
+        // in a flexible field section
+        $('.tmpl-popup').each(function () {
+            var $gravpopup = $($(this).html());
 
-        $.each(gravBlockData, function( index, value ) {
-            var blockName = index;
-            $.each(value.choices, function( index, value ) {
-                if(index){
-                    $gravpopup.find('[data-layout="' + blockName + '"]').first().parent().after('<li><a href="#" data-layout="grid" data-min="" data-max="" data-format="format-'+index+'">Grid - '+value+'</a></li>');
-                }
+            // loop through blocks and add additional choices if necessary
+            $.each(gravBlockData, function (index, value) {
+                var blockName = index;
+
+                $.each(value.choices, function(index, value) {
+                    if (index) {
+                        $gravpopup.find('[data-layout="' + blockName + '"]').first().parent().after('<li><a href="#" data-layout="grid" data-min="" data-max="" data-format="format-'+index+'">Grid - '+value+'</a></li>');
+                    }
+                });
             });
+
+            // update the "popup" tempate HTML
+            try {
+                $(this).html($gravpopup[0].outerHTML);
+            } catch (e) {
+                // safety net to prevent an uncaught error if $gravpopup is undefined
+            }
         });
-        
-        try {
-            $('.acf-field-flexible-content .tmpl-popup').html($gravpopup[0].outerHTML);
-        } catch (e) {
-            // safety net to prevent an uncaught error if $gravpopup is undefined
-        }
 
         gravBlocks.setClick();
     }
@@ -35,7 +44,6 @@ jQuery(document).ready(function($)
     gravBlocks.addFormat = function(e){
         if(e.target.dataset.layout && e.target.dataset.layout == 'grid'){
             if(e.target.dataset.format){
-                console.log(e.target.dataset.format);
                 acf.add_action('append', function($el){
                     gravBlocks.setClick();
                     var format = $el.find('[id$='+e.target.dataset.layout+'_'+e.target.dataset.format+']');
@@ -49,14 +57,11 @@ jQuery(document).ready(function($)
                 });
             }
         }
+
         if(e.target.dataset.layout){
             $(document).off('click touch', gravBlocks.addFormat);
         }
     }
 
-
-
     gravBlocks.init();
-
-
 });
